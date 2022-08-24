9036fe0a26e562b52b4cff1eb12ddcefa838866c
SSA-hub <ssapyat@gmail.com>
2022-08-24 23:29:46 +0300

    Initial commit

---
+++ MyServer/MyServer.cs
@@ -0,0 +1,25 @@
+﻿namespace MyServer;
+
+public static class Server
+{
+    private static int _count;
+    private static readonly ReaderWriterLockSlim Locker= new();
+
+    public static void AddToCount(int value)
+    {
+        Interlocked.Add(ref _count, value);
+    }
+
+    public static int GetCount()
+    {
+        Locker.EnterReadLock();
+        try
+        {
+            return _count;
+        }
+        finally
+        {
+            Locker.ExitReadLock();
+        }
+    }
+}
---
+++ TestConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using MyServer;
+
+namespace TestConsole;
+
+public static class Program
+{
+    static void Main()
+    // итерации цикла выполняются параллельно, поэтому по ним понять что-то несколько сложно,
+    // однако, итоговое значение всегда получается верным
+    {
+        Parallel.For(0, 100, i =>
+        {
+            if (i % 10 == 0) 
+                Server.AddToCount(2);
+            else
+                Console.WriteLine("Iteration: {0}; Count: {1}", i, Server.GetCount());
+        });
+        Console.WriteLine(Server.GetCount());
+        Console.WriteLine("Done.");
+    }
+}
